d09c84637472cc121ae51c7e5991d483d6f4385a
Omar Santiago <omarkenosia@gmail.com>
2021-07-12 13:54:30 -0400

    Add files via upload

---
+++ AIControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(AIControl))]
+public class AIControlEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        AIControl script = (AIControl)target;
+
+        GUI.backgroundColor = Color.yellow;
+        if (GUILayout.Button("Calc First Checkpoint") == true)
+        {
+            script.CalcFirstCheckpoint();
+        }
+    }
+}
